ce3fbf93d0c60ffb300188ca6bbf35bd6ab90253
root <root@ip-172-31-80-170.ec2.internal>
2022-07-23 12:53:48 +0000

    code

---
+++ prime.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+a=10
+until [ $a -gt 0 ]
+do 
+	echo $a
+     a = ' expr $a+1 '
+done
---
+++ shell.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo " filename :$0"
+echo " first parameter :$1"
+echo " second parameter :$2"
+echo "quated value :$@"
+echo " no of arguments :$#"
